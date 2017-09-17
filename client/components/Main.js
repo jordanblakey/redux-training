0485ed79e3bb7223a337f6313a71e3e0d8ebb987
Jordan Blakey <jordan@jordanblakey.com>
2017-09-17 18:14:48 -0500

    React router, nested components, props

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router';
-import PropTypes from 'prop-types';
 
-function Main() {
-  return (
-    <div>
-      <h1>
-        <Link to="/">Reduxstagram</Link>
-      </h1>
-    </div>
-  )
-}
+const Main = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>
+          <Link to="/">Reduxstagram</Link>
+        </h1>
+        {React.cloneElement(this.props.children, this.props)}
+      </div>
+    )
+  }
+});
 
 export default Main;
