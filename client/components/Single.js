8e3ac38fcb96060a2743b4ef05b0b17706101375
Jordan Blakey <jordan@jordanblakey.com>
2017-09-17 21:26:22 -0500

    adding and sumbitting comments on single page view

--- client/components/Single.js
+++ client/components/Single.js
@@ -7,13 +7,15 @@ import Comments from './Comments'
 const Single = React.createClass({
   render() {
     // index of the post
+    const { postId } = this.props.params;
     const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
     const post = this.props.posts[i];
+    const postComments = this.props.comments[postId] || [];
     // get us the post
     return (
       <div className="single-photo">
         <Photo i={i} post={post} {...this.props} />
-        <Comments></Comments>
+        <Comments postComments={postComments}></Comments>
       </div>
     )
   }
